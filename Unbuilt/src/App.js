5633149f1f901d7aea485c45533a0733564b6912
Baxter Hall <abaxterhall@gmail.com>
2017-03-27 14:56:44 -0400

    updates

--- Unbuilt/src/App.js
+++ Unbuilt/src/App.js
@@ -13,13 +13,13 @@ class App extends Component {
             <img className="headshot" src='image/headshot.jpg' alt="headshot" />
             <div className="briefintro">
               <p className="baxter-intro">
-                I'm currently a student in the Web Development Bootcamp at Brainstation.
+                I'm a Web Developer currently located in Toronto.
                 I'm available for Full-Time and Freelance Work.
              </p>
              <h3 className='whatIKnow'>What I Know</h3>
              <div className='col-xs-12 col-sm-10 col-sm-offset-1 col-md-6 col-md-offset-3'>
               <p className="baxter-intro">
-                HTML, CSS, Node.js, jQuery, Vanilla Javascript, ReactJS, MongoDB, PostgreSQL, and have a keen eye for design.
+                HTML, CSS, Node.js, jQuery, Vanilla Javascript, ReactJS, MongoDB, PostgreSQL, Git, and have a keen eye for design.
               </p>
               </div>            
             </div>
